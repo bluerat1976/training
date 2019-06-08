9c7357f04d8b16938a1f7e55d7f340afe2c7e275
bluerat1976 <osoblivaia76@mail.ru>
2019-06-08 15:12:22 +0300

    training process

--- js/index-5.js
+++ js/index-5.js
@@ -1,2 +1,14 @@
+//var admin;
+//var name = 'Vasiliy';
+//admin = name;
+//document.write(admin);
 
+var login = prompt('Input login');
 
+if(login == 'admin') {
+    var pass = prompt('Input password');
+} else if  (login == undefined) {
+    alert('Denide entrance');
+} else {
+    alert('I do not know you');
+}
--- js/index-8.js
+++ js/index-8.js
@@ -1,26 +1,22 @@
 function toggleContent(evt) {
     var clickEl = evt.target;
 
-    if (clickEl.classList.contains('.read-more-read-less__cta')) {
+    if (clickEl.classList.contains('read-more-read-less__cta')) {
         var currentContainer = clickEl.closest('.read-more-read-less');
+            for(var i = 0; i< currentContainer.length; i++) {
+     
+            var dots = document.getElementsByClassName('read-more-read-less__dots')[0];
+            var moreText = document.getElementsByClassName('read-more-read-less__more')[0];
+            var buttons = document.getElementsByClassName('read-more-read-less__cta');
 
-        if (currentContainer) {
-            var dots = currentContainer.getElementsByClassName('.read-more-read-less__dots');
-            var moreText = currentContainer.getElementsByClassName('.read-more-read-less__more');
-            var buttons = currentContainer.getElementsByClassName('.read-more-read-less__cta');
+            for (var j = 0; j < buttons.length; j++) {
+                buttons[j].classList.toggle('hide');
+            
 
-            for (var i = 0; i < buttons.length; i++) {
-                buttons[i].classList.toggle('.hide');
-            }
+            dots.classList.toggle('hide');
+            moreText.classList.toggle('hide');
+        }}
+    }}
+var containers = document.getElementsByClassName('read-more-read-less__container');
+    containers.addEventListener('click', toggleContent);
 
-            dots.classList.toggle('.hide');
-            moreText.classList.toggle('.hide');
-        }
-    }
-}
-
-var containers = document.getElementsByClassName('.read-more-read-less__container');
-
-if (containers.length) {
-    containers[0].addEventListener('click', toggleContent);
-}
